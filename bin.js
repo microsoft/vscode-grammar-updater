3340bfcdd1cff029db199fe3a3df23f52119f0f6
Martin Aeschlimann <martinae@microsoft.com>
2021-01-15 22:55:21 +0100

    fixes for bin

--- bin.js
+++ bin.js
@@ -1,13 +1,15 @@
+#!/usr/bin/env node
+
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-const grammarImporter = require('./');
+const grammarUpdater = require('./');
 
 if (process.argv.length <= 2) {
-    console.log('Usage: vscode-grammar-importer repoName (repoPath destinationPath)+');
+    console.log('Usage: vscode-grammar-updater repoName (repoPath destinationPath)+');
     return;
 }
 for (var i = 3; i < process.argv.length; i += 2) {
-    grammarImporter.update(process.argv[2], process.argv[i], process.argv[i + 1]);
+    grammarUpdater.update(process.argv[2], process.argv[i], process.argv[i + 1]);
 }
